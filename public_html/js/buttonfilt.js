39dd67dcad0d7426f27705153d74df7792a97728
Val <33302737+ValWV@users.noreply.github.com>
2017-11-12 03:57:51 +0300

    Update buttonfilt.js

--- public_html/js/buttonfilt.js
+++ public_html/js/buttonfilt.js
@@ -1,40 +1,23 @@
 jQuery(function ($) { 
 
-// init Isotope 
-var $grid = $('.grid').isotope({ 
-itemSelector: '.element-item', 
-layoutMode: 'packery' 
-}); 
-
-
-// filter functions 
-var filterFns = { 
-
-// show if name ends with -ium 
-n: function() { 
-var name = $(this).find('.name').text(); 
-return name; 
-} 
-}; 
-
-
-// bind filter button click 
-$('.filters-button-group').on( 'click', 'button', function() { 
-var filterValue = $( this ).attr('data-filter'); 
-// use filterFn if matches value 
-filterValue = filterFns[ filterValue ] || filterValue; 
-$grid.isotope({ filter: filterValue }); 
-}); 
-// change is-checked class on buttons 
-$('.button-group').each( function( i, buttonGroup ) { 
-var $buttonGroup = $( buttonGroup ); 
-$buttonGroup.on( 'click', 'button', function() { 
-$buttonGroup.find('.is-checked').removeClass('is-checked'); 
-$( this ).addClass('is-checked'); 
-}); 
-}); 
+// init Isotope
+var $grid = $('.grid').isotope({
+  itemSelector: '.element-item',
+  layoutMode: 'packery'
 });
-
+// filter functions
+var filterFns = {
+  // show if number is greater than 50
+  numberGreaterThan50: function() {
+    var number = $(this).find('.number').text();
+    return parseInt( number, 10 ) > 50;
+  },
+  // show if name ends with -ium
+  ium: function() {
+    var name = $(this).find('.name').text();
+    return name.match( /ium$/ );
+  }
+};
 // bind filter on select change
 $('.filters-select').on( 'change', function() {
   // get filter value from option value
@@ -42,4 +25,5 @@ $('.filters-select').on( 'change', function() {
   // use filterFn if matches value
   filterValue = filterFns[ filterValue ] || filterValue;
   $grid.isotope({ filter: filterValue });
+});
 });
